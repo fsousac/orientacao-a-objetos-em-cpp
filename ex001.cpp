9065415da1f8ac0baf3ce1981ca209227c142114
Felipe de Sousa <211061707@aluno.unb.br>
2022-11-13 18:29:04 -0300

    using cin/cout to manipulate variables

--- ex001.cpp
+++ ex001.cpp
@@ -1,11 +1,16 @@
-//
-// Created by Felipe on 13/11/2022.
-//
+//#include <iostream.h>
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main(){
-    cout<< "a"<<endl;
+    string nome;
+    cout<< "Qual o seu nome?\n";
+    cin >> nome;
+    cout << "Olá, " << nome<< endl;
+    cout << 10 + 8 <<endl;
+    cout << hex <<10 + 8 <<endl;
+    cout << oct <<10 + 8 <<endl;
+
     return 0;
 }
